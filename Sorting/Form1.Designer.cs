07617ac3522bf36ef343398b7e1ea74ae176f17d
KidFriendly <47805062+KidFriendly@users.noreply.github.com>
2020-08-05 00:59:02 -0400

    Initial commit

---
+++ Sorting/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboSort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textRoot = new System.Windows.Forms.TextBox();
+            this.textPrefix = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(107, 316);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(868, 127);
+            this.btnSort.TabIndex = 0;
+            this.btnSort.Text = "Sort!";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(577, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Welcome to the auto sorting program.\r\nPlease select the type of sort requested an" +
+    "d the root folder you would like to sort.";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // comboSort
+            // 
+            this.comboSort.FormattingEnabled = true;
+            this.comboSort.Items.AddRange(new object[] {
+            "Sort JPG",
+            "Sort Video (mp4)",
+            "Sort PDF",
+            "Sort Custom (Insert File Type)"});
+            this.comboSort.Location = new System.Drawing.Point(394, 111);
+            this.comboSort.Name = "comboSort";
+            this.comboSort.Size = new System.Drawing.Size(253, 28);
+            this.comboSort.TabIndex = 2;
+            this.comboSort.SelectedIndexChanged += new System.EventHandler(this.comboSort_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(484, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SortType";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(667, 245);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(37, 26);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "...";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(475, 222);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Root Folder";
+            // 
+            // textRoot
+            // 
+            this.textRoot.Location = new System.Drawing.Point(394, 245);
+            this.textRoot.Name = "textRoot";
+            this.textRoot.Size = new System.Drawing.Size(253, 26);
+            this.textRoot.TabIndex = 6;
+            this.textRoot.TextChanged += new System.EventHandler(this.textRoot_TextChanged);
+            // 
+            // textPrefix
+            // 
+            this.textPrefix.Location = new System.Drawing.Point(394, 172);
+            this.textPrefix.Name = "textPrefix";
+            this.textPrefix.Size = new System.Drawing.Size(253, 26);
+            this.textPrefix.TabIndex = 7;
+            this.toolTip1.SetToolTip(this.textPrefix, "Leave empty for no prefix");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(486, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Prefix (?)";
+            this.toolTip1.SetToolTip(this.label4, "Leave empty for no prefix");
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 522);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textPrefix);
+            this.Controls.Add(this.textRoot);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboSort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSort);
+            this.Name = "Form1";
+            this.Text = "AutoSorting Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboSort;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textRoot;
+        private System.Windows.Forms.TextBox textPrefix;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Sorting/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static class Prompt
+        {
+            public static string ShowDialog(string text, string caption)
+            {
+                Form prompt = new Form();
+                prompt.Width = 500;
+                prompt.Height = 500;
+                prompt.Text = caption;
+                Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+                TextBox inputBox = new TextBox() { Left = 50, Top = 50, Width = 400 };
+                Button confirmation = new Button() { Text = "Ok", Left = 350, Width = 100, Top = 70 };
+                confirmation.Click += (sender, e) => { prompt.Close(); };
+                prompt.Controls.Add(confirmation);
+                prompt.Controls.Add(textLabel);
+                prompt.Controls.Add(inputBox);
+                prompt.ShowDialog();
+                return inputBox.Text;
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Path = null;
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+                Path = fbd.SelectedPath;
+            textRoot.Text = Path;
+        }
+
+        private void toolTip1_Popup(object sender, PopupEventArgs e)
+        {
+
+        }
+
+        private void comboSort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            string sortValue = comboSort.Text;
+            string folderLocation = textRoot.Text;
+
+            if (sortValue.Equals("Sort JPG")){
+                string prefix = textPrefix.Text;
+                if (string.IsNullOrWhiteSpace(textPrefix.Text))
+                {
+                    prefix = " ";
+                }
+                DirectoryInfo d = new DirectoryInfo(folderLocation);
+                FileInfo[] infos = d.GetFiles();
+                foreach (FileInfo f in infos)
+                {
+                    if (!File.Exists(folderLocation + "\\" + prefix + counter.ToString() + ".jpg"))
+                    {
+                        File.Move(f.FullName, Path.Combine(f.DirectoryName, prefix + counter.ToString() + ".jpg"));
+                        counter++;
+                    }
+                    else
+                        counter++;
+                    
+                }
+
+            }
+
+            else if (sortValue.Equals("Sort Video (mp4)"))
+            {
+                string prefix = textPrefix.Text;
+                if (string.IsNullOrWhiteSpace(textPrefix.Text))
+                {
+                    prefix = " ";
+                }
+                DirectoryInfo d = new DirectoryInfo(folderLocation);
+                FileInfo[] infos = d.GetFiles();
+                foreach (FileInfo f in infos)
+                {
+                    if (!File.Exists(folderLocation + "\\" + prefix + counter.ToString() + ".mp4"))
+                    {
+                        File.Move(f.FullName, Path.Combine(f.DirectoryName, prefix + counter.ToString() + ".mp4"));
+                        counter++;
+                    }
+                    else
+                        counter++;
+                }
+
+            }
+            else if (sortValue.Equals("Sort PDF"))
+            {
+                string prefix = textPrefix.Text;
+                if (string.IsNullOrWhiteSpace(textPrefix.Text))
+                {
+                    prefix = " ";
+                }
+                DirectoryInfo d = new DirectoryInfo(folderLocation);
+                FileInfo[] infos = d.GetFiles();
+                foreach (FileInfo f in infos)
+                {
+                    if (!File.Exists(folderLocation + "\\" + prefix + counter.ToString() + ".pdf"))
+                    {
+                        File.Move(f.FullName, Path.Combine(f.DirectoryName, prefix + counter.ToString() + ".pdf"));
+                        counter++;
+                    }
+                    else
+                        counter++;
+                }
+
+            }
+
+            else
+            {
+                string fileType = Prompt.ShowDialog("Custom File Type", "Input a file type");
+                string prefix = textPrefix.Text;
+                if (string.IsNullOrWhiteSpace(textPrefix.Text))
+                {
+                    prefix = " ";
+                }
+                DirectoryInfo d = new DirectoryInfo(folderLocation);
+                FileInfo[] infos = d.GetFiles();
+                foreach (FileInfo f in infos)
+                {
+                    if (!File.Exists(folderLocation + "\\" + prefix + counter.ToString() + fileType))
+                    {
+                        File.Move(f.FullName, Path.Combine(f.DirectoryName, prefix + counter.ToString() + fileType));
+                        counter++;
+                    }
+                    else
+                        counter++;
+                }
+
+            }
+
+            MessageBox.Show("Complete");
+
+
+        }
+
+        private void textRoot_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
